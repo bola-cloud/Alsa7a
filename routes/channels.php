462bd55d82bfeeae56ac0cf32edff00d623636ec
bola-cloud <bola.ishak41@gmail.com>
2026-01-05 23:00:17 +0200

    chat channel

--- routes/channels.php
+++ routes/channels.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Broadcast;
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
+
+Broadcast::channel('chat.{conversationId}', function ($user, $conversationId) {
+    $conversation = \App\Models\Conversation::find($conversationId);
+    return $conversation && ($conversation->user_one_id == $user->id || $conversation->user_two_id == $user->id);
+});
