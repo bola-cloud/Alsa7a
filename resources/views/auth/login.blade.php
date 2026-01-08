1f1c395836fb7dc61b226a05998cee9ba7e7b584
bola-cloud <bola.ishak41@gmail.com>
2026-01-08 11:19:16 +0200

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html class="loading" lang="en">
+
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,37 +8,45 @@
     <title>{{ config('app.name', 'Alsa7a') }} | Login</title>
     <link rel="apple-touch-icon" href="{{asset('app-assets/images/ico/apple-icon-120.png')}}">
     <link rel="shortcut icon" type="image/x-icon" href="{{asset('app-assets/images/logo.jpeg')}}">
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Quicksand:300,400,500,700" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Quicksand:300,400,500,700"
+        rel="stylesheet">
     <link href="https://maxcdn.icons8.com/fonts/line-awesome/1.1/css/line-awesome.min.css" rel="stylesheet">
-    
+
     <!-- BEGIN VENDOR CSS-->
     @if (app()->getLocale() == 'ar')
         <link rel="stylesheet" type="text/css" href="{{asset('app-assets/css-rtl/vendors.css')}}">
         <link rel="stylesheet" type="text/css" href="{{asset('app-assets/css-rtl/app.css')}}">
-        <link rel="stylesheet" type="text/css" href="{{asset('app-assets/css-rtl/core/menu/menu-types/vertical-content-menu.css')}}">
+        <link rel="stylesheet" type="text/css"
+            href="{{asset('app-assets/css-rtl/core/menu/menu-types/vertical-content-menu.css')}}">
         <link rel="stylesheet" type="text/css" href="{{asset('app-assets/css-rtl/core/colors/palette-gradient.css')}}">
         <link rel="stylesheet" type="text/css" href="{{asset('app-assets/css-rtl/custom-rtl.css')}}">
         <style>
-            body { font-family: 'Cairo', sans-serif !important; }
+            body {
+                font-family: 'Cairo', sans-serif !important;
+            }
         </style>
     @else
         <link rel="stylesheet" type="text/css" href="{{asset('app-assets/css/vendors.css')}}">
         <link rel="stylesheet" type="text/css" href="{{asset('app-assets/css/app.css')}}">
-        <link rel="stylesheet" type="text/css" href="{{asset('app-assets/css/core/menu/menu-types/vertical-content-menu.css')}}">
+        <link rel="stylesheet" type="text/css"
+            href="{{asset('app-assets/css/core/menu/menu-types/vertical-content-menu.css')}}">
         <link rel="stylesheet" type="text/css" href="{{asset('app-assets/css/core/colors/palette-gradient.css')}}">
     @endif
     <style>
         /* Override background to solid color matching admin panel theme or user request */
         body.blank-page {
             background-image: none !important;
-            background-color: #f4f5fa; /* A clean, light grey often used in panels */
+            background-color: #f4f5fa;
+            /* A clean, light grey often used in panels */
         }
     </style>
     <!-- END VENDOR CSS-->
 </head>
+
 <body class="vertical-layout vertical-content-menu 1-column bg-full-screen-image menu-expanded blank-page blank-page"
-      data-open="click" data-menu="vertical-content-menu" data-col="1-column">
-    
+    data-open="click" data-menu="vertical-content-menu" data-col="1-column">
+
     <div class="app-content content">
         <div class="content-wrapper">
             <div class="content-header row"></div>
@@ -49,7 +58,8 @@
                                 <div class="card-header border-0">
                                     <div class="card-title text-center">
                                         <div class="p-1">
-                                            <img src="{{asset('app-assets/images/logo.jpeg')}}" alt="branding logo" style="max-height: 80px;">
+                                            <img src="{{asset('app-assets/images/logo.jpeg')}}" alt="branding logo"
+                                                style="max-height: 80px;">
                                         </div>
                                     </div>
                                     <h6 class="card-subtitle line-on-side text-muted text-center font-small-3 pt-2">
@@ -58,16 +68,20 @@
                                 </div>
                                 <div class="card-content">
                                     <div class="card-body">
-                                        <form class="form-horizontal form-simple" action="{{ route('login') }}" method="POST" novalidate>
+                                        <form class="form-horizontal form-simple" action="{{ route('login') }}"
+                                            method="POST" novalidate>
                                             @csrf
                                             <fieldset class="form-group position-relative has-icon-left mb-0">
-                                                <input type="text" class="form-control form-control-lg input-lg" id="phone" name="phone" placeholder="Your Phone Number" required>
+                                                <input type="text" class="form-control form-control-lg input-lg"
+                                                    id="email" name="email" placeholder="Email or Phone" required>
                                                 <div class="form-control-position">
                                                     <i class="ft-user"></i>
                                                 </div>
                                             </fieldset>
                                             <fieldset class="form-group position-relative has-icon-left">
-                                                <input type="password" class="form-control form-control-lg input-lg" id="user-password" name="password" placeholder="Enter Password" required>
+                                                <input type="password" class="form-control form-control-lg input-lg"
+                                                    id="user-password" name="password" placeholder="Enter Password"
+                                                    required>
                                                 <div class="form-control-position">
                                                     <i class="la la-key"></i>
                                                 </div>
@@ -75,17 +89,20 @@
                                             <div class="form-group row">
                                                 <div class="col-md-6 col-12 text-center text-md-left">
                                                     <fieldset>
-                                                        <input type="checkbox" id="remember-me" name="remember" class="chk-remember">
+                                                        <input type="checkbox" id="remember-me" name="remember"
+                                                            class="chk-remember">
                                                         <label for="remember-me"> Remember Me</label>
                                                     </fieldset>
                                                 </div>
                                                 <div class="col-md-6 col-12 text-center text-md-right">
                                                     @if (Route::has('password.request'))
-                                                        <a href="{{ route('password.request') }}" class="card-link">Forgot Password?</a>
+                                                        <a href="{{ route('password.request') }}" class="card-link">Forgot
+                                                            Password?</a>
                                                     @endif
                                                 </div>
                                             </div>
-                                            <button type="submit" class="btn btn-info btn-lg btn-block"><i class="ft-unlock"></i> Login</button>
+                                            <button type="submit" class="btn btn-info btn-lg btn-block"><i
+                                                    class="ft-unlock"></i> Login</button>
                                         </form>
                                     </div>
                                 </div>
@@ -95,7 +112,8 @@
                                             <p class="float-sm-left text-center m-0">
                                                 <a href="{{ route('register') }}" class="card-link">Recover password</a>
                                             </p>
-                                            <p class="float-sm-right text-center m-0">New to Alsa7a? <a href="{{ route('register') }}" class="card-link">Sign Up</a></p>
+                                            <p class="float-sm-right text-center m-0">New to Alsa7a? <a
+                                                    href="{{ route('register') }}" class="card-link">Sign Up</a></p>
                                         @endif
                                     </div>
                                 </div>
@@ -113,4 +131,5 @@
     <script src="{{asset('app-assets/js/core/app.js')}}" type="text/javascript"></script>
     <!-- END VENDOR JS-->
 </body>
+
 </html>
