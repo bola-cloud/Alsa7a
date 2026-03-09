b002800a20cffff6ecb0c2ea69b555055b7e565b
bola-cloud <bola.ishak41@gmail.com>
2026-03-09 17:14:25 +0200

    Include message body in MessageNotification push and DB alerts

--- app/Notifications/MessageNotification.php
+++ app/Notifications/MessageNotification.php
@@ -28,7 +28,7 @@ class MessageNotification extends Notification
     {
         return [
             'title' => 'New Message',
-            'body' => "You have a new message from {$this->sender->name}",
+            'body' => "{$this->sender->name}: {$this->message->body}",
             'type' => 'message',
             'conversation_id' => $this->message->conversation_id,
             'message_id' => $this->message->id,
@@ -41,8 +41,8 @@ class MessageNotification extends Notification
         return [
             'title' => ['en' => 'New Message', 'ar' => 'رسالة جديدة'],
             'message' => [
-                'en' => "You have a new message from {$this->sender->name}",
-                'ar' => "لديك رسالة جديدة من {$this->sender->name}"
+                'en' => "{$this->sender->name}: {$this->message->body}",
+                'ar' => "{$this->sender->name}: {$this->message->body}"
             ],
             'data' => [
                 'type' => 'message',
